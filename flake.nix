ae832689b7285527653b5f58b42e0470cbba54d9
dorkeline <git@cafebabe.dev>
2025-04-01 23:46:15 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+
+      flake.overlays.default = final: prev: {
+        sudo-nx-ngwords = prev.sudo.overrideAttrs (old: {
+          configureFlags =
+            old.configureFlags
+            ++ [
+              "--with-insults"
+              "--with-goons-insults"
+            ];
+
+          patches =
+            (old.patches or [])
+            ++ [
+              ./src/0001-Replace-the-Goons-insults-with-insults-from-the-Nint.patch
+            ];
+        });
+      };
+
+      perSystem = {pkgs, ...}: {
+        packages = let
+          myPkgs = self.overlays.default pkgs pkgs;
+        in
+          myPkgs // {default = myPkgs.sudo-nx-ngwords;};
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
